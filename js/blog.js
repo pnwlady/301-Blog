d05f1303f35a2f22e6fbc63f4ca1bd62ab8196c5
Heidi Laursen <heidilaursen@yahoo.com>
2015-12-01 15:26:20 -0800

    added sorting functions for publishedOn

--- js/blog.js
+++ js/blog.js
@@ -1,2 +1,30 @@
 var blog = {};
 blog.articles = [];
+
+//this code is borrowing from class demo/mirandalily
+blog.sortRawData = function() {
+  blog.rawData.sort(function(a,b) {
+    if (a.publishedOn > b.publishedOn) {
+      return -1;
+    }
+    if (a.publishedOn < b.publishedOn) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+blog.createArticles = function() {
+  for (var i = 0; i < blog.rawData.length; i += 1) {
+    var article = new Article(blog.rawData[i]);
+    blog.articles.push(article);
+    article.toHTML();
+  }
+};
+
+// blog.createArticles = funtion() {
+//   for (var i = 0; i < blog.rawData.length; i++) {
+//     var temp = new Article (blog.rawData[i]);
+//     temp.toHTML();
+//   }
+// }
