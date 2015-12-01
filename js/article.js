aca95b75da6e990f1ecbbffee3a82d9831937514
Heidi Laursen <heidilaursen@yahoo.com>
2015-11-30 18:05:27 -0800

    adding jQuery for template

--- js/article.js
+++ js/article.js
@@ -17,9 +17,14 @@ Article.prototype.toHTML = function () {
   //select template element
   var $temp1 = $('#template').clone();
   //clone the element
-$temp1.find('.title').html(this.title);
+  $temp1.find('.title').html(this.title);
   //change the author info
+  $temp1.find('.author').html(this.author);
+  $temp1.find('.authorUrl').html(this.authorUrl);
   //change body
+  $temp1.find('.body').html(this.body);
   //change publishedOn
+  $temp1.find('.publishedOn').html.(this.publishedOn);
   //append back to html page
+  
 }
