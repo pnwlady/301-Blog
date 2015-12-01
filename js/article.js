ca77325692cf336f722819a7a5de8bf015fd901c
Heidi Laursen <heidilaursen@yahoo.com>
2015-11-30 18:02:48 -0800

    fixing template

--- js/article.js
+++ js/article.js
@@ -8,9 +8,18 @@ var Article = function(property) {
 }
 
 Article.prototype.toHTML = function () {
-  return '<article>' +
-  '<h1>' + this.title + '</h1>' +
-  '<section>' + this.author + this.authorUrl + '</section>' +
-  '<p>' + this.body + '</p>' +
-  this.publishedOn + '</article>';
+  // return '<article>' +
+  // '<h1>' + this.title + '</h1>' +
+  // '<section>' + this.author + this.authorUrl + '</section>' +
+  // '<p>' + this.body + '</p>' +
+  // this.publishedOn + '</article>';
+
+  //select template element
+  var $temp1 = $('#template').clone();
+  //clone the element
+$temp1.find('.title').html(this.title);
+  //change the author info
+  //change body
+  //change publishedOn
+  //append back to html page
 }
