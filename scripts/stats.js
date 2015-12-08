8c800b9c05c9eb81b29d2196a053732750e84c92
Heidi Laursen <heidilaursen@yahoo.com>
2015-12-08 15:21:40 -0800

    working on mapping, reducing, spitting and splicing

--- scripts/stats.js
+++ scripts/stats.js
@@ -2,31 +2,98 @@
 var stats = {};
 
 //getting data from blogArticles
-$.get('scripts/blogArticles.js', function(data) {
-  stats.data = data;
+// $.get('scripts/blogArticles.js', function(data) {
+//   stats.data = data;
+// });
+
+//getting data from hackerIpsum
+$.get('scripts/hackerIpsum.json', function(data) {
+  stats.string = data;
+  stats.table = stats.string
+  .split('<tr>') //split string into rows
+  .slice(1) //remove first empty result
+  .map(function(row) {
+    //loop through the rows using 'map'
+    return row
+      .slice(0, -5) //removing closing </tr>
+      .split('<td>') //split row string into columns
+      .slice(1) //remove first empty column
+      .map(function(col) {
+        //loop through the columns using 'map'
+        return col.slice(0, -5); //remove closing </td>
+      });
+  })
+  .reduce(function(acc, obj) {
+    var rank = obj.rank;
+    var prevRank = obj.prevRank;
+    var name = obj.name;
+    if (rank < prevRank) {
+      acc.push({
+        rank: rank,
+        prevRank: prevRank,
+        name: name
+      });
+      return acc;
+    };
+  });
+  console.log(table);
 });
 
-//getting datat from hackerIpsum
-$.get('scripts/hackerIpsum.json', function(data) {
-  stats.data = data;
-});
-//
+
 // stats.getAuthors = function(num)
-//
 // //use code from jonathan's example in class
+// stats.getArticles = function(num)
+//get data with map and forEach
+//splice data by spaces
+//count data
+//return quantity
+
+//ivan's code example
+$.get('scripts/hackerIpsum.json', scraper);
+function scraper(data) {
+  var htmlString = data;
+  var table = htmlString
+      .split('<tr>') //split string into rows
+      .slice(1) //remove first empty result
+      .map(function(row) {
+        //loop through the rows using 'map'
+        return row
+          .slice(0, -5) //remove the closing </tr>
+          .split('<td>') //split row string into columns
+          .slice(1) //remove first empty column
+          .map(function(col) {
+          //loop through the columns using 'map'
+          //remove the closing </td>
+            return col.slice(0, -5);
+          });
+      })
+      .reduce(function(acc, obj) {
+        var rank = obj.rank;
+        var prevRank = obj.prevRank;
+        var name = obj.name;
+        if (rank < prevRank) {
+          acc.push({
+            rank: rank,
+            prevRank: prevRank,
+            name: name
+          });
+        }
+        return acc;
+      });
+  console.log(table);
+}
+
+
+
+
+
+
+
+
+
+
+
 //
-// stats.getArticles = function(num) {
-// };
-//
-//
-//
-// //get data with map and forEach
-//
-// //splice data by spaces
-//
-// //count data
-//
-// //return quantity
 //
 // //Code from class - Brook recommends naming the function
 // //create an object (might need new file)
@@ -87,5 +154,4 @@ $.get('scripts/hackerIpsum.json', function(data) {
 //         }
 //         return acc;
 //       });
-//   console.log(table);
-// }
+// };
